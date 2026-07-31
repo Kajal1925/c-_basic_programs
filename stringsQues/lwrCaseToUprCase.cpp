de12861b297f9ba4f12642086b7e65f7c8dcc7bb
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-31 18:30:29 +0530

    Add files via upload

---
+++ stringsQues/lwrCaseToUprCase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char str[100];
+    int i = 0;
+    
+    cout<<"Enter the string: ";
+    cin.getline(str, 100);
+
+    while(str[i] != '\0'){
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = str[i] -32;
+        }
+        i++;
+    }
+    cout<<"Uppercase string: "<<str;
+    return 0;
+}
